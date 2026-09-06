2d30fb7968e91e8b03348d5927f5de453aec0fb9
gamer2236 <gamer2236@users.noreply.github.com>
2026-09-06 15:31:21 +0000

    Jump: books list, inline chapters, Select Verse grid (no Go)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_STATIC = "ttb-static-v9";
-const CACHE_TEXT = "ttb-text-v9";
+const CACHE_STATIC = "ttb-static-v10";
+const CACHE_TEXT = "ttb-text-v10";
 const PRECACHE = ["./","./index.html","./manifest.webmanifest","./assets/index.js","./assets/routes.js","./assets/styles.css"];
 self.addEventListener("install", (e) => e.waitUntil(caches.open(CACHE_STATIC).then((c)=>c.addAll(PRECACHE)).then(()=>self.skipWaiting())));
 self.addEventListener("activate", (e) => e.waitUntil(caches.keys().then((keys)=>Promise.all(keys.filter((k)=>k!==CACHE_STATIC&&k!==CACHE_TEXT).map((k)=>caches.delete(k)))).then(()=>self.clients.claim())));
